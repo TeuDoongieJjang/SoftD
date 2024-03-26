46b81bc8c620f98778d6bbcf67d223dc31b54c4d
TeuDoongieJjang <louisekurt117@gmail.com>
2024-03-27 01:54:52 +0800

    Ready

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -7,7 +7,7 @@ const dbURI = process.env.MONGO_DB_URI
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbURI.toString)
+        await mongoose.connect(dbURI.toString())
         console.log(`Connected to MongoDB`)
     } catch (error) {
         console.log(`Error Connection MongoDB: ${error}`)
