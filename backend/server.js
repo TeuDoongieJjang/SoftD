84dd0d9ed8bcb5cd6f35619e21c5e449459c3c24
TeuDoongieJjang <louisekurt117@gmail.com>
2024-03-27 01:16:01 +0800

    Frontend Completed

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,25 @@ import express from 'express'
 import dotenv from 'dotenv'
 import directRoute from './routes/directRoute.js'
 import connectDB from './db/connectDB.js'
-
+import path from 'path'
 
 dotenv.config()
 
-const port = process.env.PORT || 8700
+const port = process.env.PORT || 2700
+
+const __dirname = path.resolve()
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/', directRoute)
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")))
+
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.listen(port, async () => {
    try {
